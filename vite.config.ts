5a62a2e71be96ef3a80cb675e2d7e7efebfeed32
Shakir Ullah <sullah@ariasystems.com>
2025-10-26 17:21:46 +0500

    feat: refactor project structure and update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "",
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true, // generates index.d.ts
+            outDir: "dist", // ensures types go to dist/
+        }),
+    ],
+    build: {
+        lib: {
+            entry: "src/index.ts",
+            name: "ReactPerformify",
+            fileName: (format) => `react-performify.${format}.js`,
+        },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
+    },
+});
